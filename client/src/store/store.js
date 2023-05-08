477c67e7767116cae5ccefa46c671d06bcd10241
Eugene_dev <lointainyyy@gmail.com>
2023-05-08 12:52:58 +0300

    added theme, sidebar logic

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,11 +3,13 @@ import { configureStore } from '@reduxjs/toolkit'
 /* Reducers */
 import noteReducer from './reducers/noteSlice'
 import modalReducer from './reducers/modalSlice'
+import uiReducer from './reducers/uiSlice'
 
 export const store = configureStore({
 	reducer: {
 		note: noteReducer,
-		modal: modalReducer
+		modal: modalReducer,
+		ui: uiReducer
 	}
 })
 
