fa85125e5872b30214470907360a06e39f2522a1
Eugene_dev <lointainyyy@gmail.com>
2023-05-08 12:54:15 +0300

    configured app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,36 @@
-import React from 'react'
+/* Styles */
+import './App.scss'
+
+import { Modals, Workspace } from './components'
+import { Header, Sidebar } from './layout'
+
+/* Hooks */
+import useTheme from './hooks/useTheme'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { setTheme } from './store/reducers/uiSlice'
 
 export default function App() {
-	return <div>App</div>
+	const dispatch = useDispatch()
+
+	// Get name of theme from localStorage or set default
+	const { userTheme } = useTheme()
+
+	// Status theme
+	const { colorTheme: theme } = useSelector((store) => store.ui)
+
+	useEffect(() => {
+		// Set theme name to store
+		userTheme && dispatch(setTheme(userTheme))
+	}, [dispatch])
+
+	return (
+		<div className={`App ${theme}`}>
+			<Modals />
+			<Header />
+			<Sidebar />
+			<Workspace />
+		</div>
+	)
 }
 
