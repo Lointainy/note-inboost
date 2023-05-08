5e380eaf6ee6ec7a85a309b6d21294f466b293c6
Eugene_dev <lointainyyy@gmail.com>
2023-05-08 18:17:56 +0300

    added logic for add new note

--- client/src/store/reducers/noteSlice.js
+++ client/src/store/reducers/noteSlice.js
@@ -1,16 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { v4 as uuidv4 } from 'uuid'
 
 const initialState = {
 	notes: []
 }
 
+const defaultNote = {
+	_id: '',
+	title: 'New note',
+	date: '',
+	filter: ['default']
+}
+
 export const noteSlice = createSlice({
 	name: 'note',
 	initialState,
-	reducers: {}
+	reducers: {
+		addNote: (state) => {
+			state.notes = [...state.notes, { ...defaultNote, _id: uuidv4() }]
+			console.log(JSON.stringify(state.notes))
+		}
+	}
 })
 
-export const {} = noteSlice.actions
+export const { addNote } = noteSlice.actions
 
 export default noteSlice.reducer
 
