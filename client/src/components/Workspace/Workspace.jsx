462575e2a1a58c13cf03cfb455d0f124b154a3f3
Eugene_dev <lointainyyy@gmail.com>
2023-05-09 14:44:36 +0300

    deleted component

--- client/src/components/Workspace/Workspace.jsx
+++
@@ -1,7 +0,0 @@
-/* Styles */
-import style from './Workspace.module.scss'
-
-export default function Workspace({ fullsize }) {
-	return <div className={`${style.workspace} ${fullsize ? style.fullsize : ''}`}>Workspace</div>
-}
-
