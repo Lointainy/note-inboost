82a36634925158c1fc7c7994fb5d30de50cac137
Eugene_dev <lointainyyy@gmail.com>
2023-05-09 13:10:18 +0300

    added page

---
+++ client/src/pages/NotePage/NotePage.jsx
@@ -0,0 +1,4 @@
+export default function NotePage() {
+	return <div>NotePage</div>
+}
+
---
+++ client/src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as NotePage } from './NotePage/NotePage'
+export { default as LoginPage } from './LoginPage/LoginPage'
+export { default as SignUpPage } from './SignUpPage/SignUpPage'
+export { default as NotFoundPage } from './NotFoundPage/NotFoundPage'
+
