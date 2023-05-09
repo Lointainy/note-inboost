040746c395550cb9b788d5e1deca9d7da51b9a63
Eugene_dev <lointainyyy@gmail.com>
2023-05-09 15:42:56 +0300

    added and configured auth logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import { useEffect } from 'react'
 
 /* Routes */
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 
 /* Hooks */
 import useTheme from './hooks/useTheme'
+import authWrapper from './hooks/authWrapper'
 
 /* Store */
 import { useDispatch, useSelector } from 'react-redux'
@@ -20,34 +21,50 @@ import { NotFoundPage, LoginPage, SignUpPage, NotePage } from './pages'
 import { Modals } from './components'
 import { Header, Sidebar } from './layout'
 
-export default function App() {
+export const App = () => {
 	const dispatch = useDispatch()
+
+	// Routes
+	const { pathname } = useLocation()
 
 	// Get name of theme from localStorage or set default
 	const { userTheme } = useTheme()
 
-	// Status theme
 	const { colorTheme: theme, sidebar } = useSelector((store) => store.ui)
+	const userLogined = useSelector((store) => store.auth.login)
 
 	useEffect(() => {
 		// Set theme name to store
 		userTheme && dispatch(setTheme(userTheme))
 	}, [dispatch, userTheme])
+	if (userLogined && !pathname.includes('/login')) {
+		return (
+			<div className={`App ${theme}`}>
+				<Modals />
+				<Header />
+				{sidebar && <Sidebar />}
+				<div className={`page-content ${!sidebar ? 'fullsize' : ''}`}>
+					<Routes>
+						<Route path={'note/:noteId'} element={<NotePage />} />
+						<Route path={'*'} element={<NotFoundPage />} />
+					</Routes>
+				</div>
+
+				{/* <Workspace fullsize={!sidebar} /> */}
+			</div>
+		)
+	}
 
 	return (
 		<div className={`App ${theme}`}>
-			<Modals />
-			<Header />
-			{sidebar && <Sidebar />}
-			<div className={`page-content ${!sidebar ? 'fullsize' : ''}`}>
-				<Routes>
-					<Route path={'note/:noteId'} element={<NotePage />} />
-					<Route path={'*'} element={<NotFoundPage />} />
-				</Routes>
-			</div>
-
-			{/* <Workspace fullsize={!sidebar} /> */}
+			<Routes>
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/signup" element={<SignUpPage />} />
+				<Route path="*" element={<NotFoundPage />} />
+			</Routes>
 		</div>
 	)
 }
+
+export default authWrapper(App)
 
