2d763a50ada3c0bc58b56debe6201649a4f8c039
Eugene_dev <lointainyyy@gmail.com>
2023-05-09 17:59:47 +0300

    added logic for note

--- server/controllers/noteController.js
+++ server/controllers/noteController.js
@@ -21,23 +21,67 @@ const getNotes = async (req, res) => {
 	} catch (error) {
 		res.status(500).json({ msg: error.message })
 	}
-
-	// try {
-
-	// 	// if (!notes) {
-	// 	// 	console.log(1)
-	// 	// }
-
-	// 	// if (userData) {
-	// 	// 	res.status(200).json(notes)
-	// 	// } else {
-	// 	// 	res.status(404).json({ msgg: 'No notes' })
-	// 	// }
-	// } catch (error) {
-	// 	res.status(500).json({ msg: error.message })
-	// 	console.log(error)
-	// }
 }
 
-module.exports = { getNotes }
+const createNote = async (req, res) => {
+	let user_id = req.user._id
+	let newNote = req.body
+
+	try {
+		let userData = await Note.findOne({ user_id })
+		userData.notes.push(newNote)
+
+		await userData.save()
+		res.status(200).json(`User create new note`)
+	} catch (error) {
+		res.status(500).json({ msg: error.message })
+		console.log(error)
+	}
+}
+
+const updateNote = async (req, res) => {
+	let user_id = req.user._id
+	let note_id = req.params.id
+	let updatedNote = req.body
+
+	try {
+		let userData = await Note.findOne({ user_id })
+
+		let noteToUpdate = userData.notes.findIndex((n) => n._id == note_id)
+
+		if (noteToUpdate >= 0) {
+			userData = await Note.findOneAndUpdate({ user_id, 'notes._id': note_id }, { $set: { 'notes.$': { ...updatedNote, _id: note_id } } })
+			res.status(200).json({ msg: `Note was Updated` })
+		} else {
+			res.status(400).json({ msg: `No such Note with ${note_id}` })
+		}
+	} catch (error) {
+		res.status(500).json({ msg: error.message })
+		console.log(error)
+	}
+}
+
+const deleteNote = async (req, res) => {
+	let user_id = req.user._id
+	let note_id = req.params.id
+
+	try {
+		let userData = await Note.findOne({ user_id })
+
+		let noteToRemove = userData.notes.findIndex((n) => n._id == note_id)
+
+		if (noteToRemove >= 0) {
+			userData.notes.pull({ _id: note_id })
+			await userData.save()
+			res.status(200).json({ msg: `Note was deleted` })
+		} else {
+			res.status(400).json({ msg: `No such Note with ${note_id}` })
+		}
+	} catch (error) {
+		res.status(500).json({ msg: error.message })
+		console.log(error)
+	}
+}
+
+module.exports = { getNotes, createNote, updateNote, deleteNote }
 
--- server/routes/note.js
+++ server/routes/note.js
@@ -3,11 +3,14 @@ const express = require('express')
 const router = express.Router()
 const requireAuth = require('../middleware/requireAuth')
 
-const { getNotes } = require('../controllers/noteController')
+const { getNotes, createNote, updateNote, deleteNote } = require('../controllers/noteController')
 
 router.use(requireAuth)
 
 router.get('/', getNotes)
+router.post('/', createNote)
+router.patch('/:id', updateNote)
+router.delete('/:id', deleteNote)
 
 module.exports = router
 
