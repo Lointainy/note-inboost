cf4d73f4964b710aa964a905d8552e7749155192
Eugene_dev <lointainyyy@gmail.com>
2023-05-09 16:59:35 +0300

    added icons

--- client/src/index.js
+++ client/src/index.js
@@ -21,9 +21,9 @@ import './styles/main.scss'
 
 /* ICONS */
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSearch, faBars, faPlus, faSun, faMoon, faCircleXmark, faCheck } from '@fortawesome/free-solid-svg-icons'
+import { faSearch, faBars, faPlus, faSun, faMoon, faCircleXmark, faCheck, faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faSearch, faBars, faPlus, faSun, faMoon, faCircleXmark, faCheck)
+library.add(faSearch, faBars, faPlus, faSun, faMoon, faCircleXmark, faCheck, faRightFromBracket)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
