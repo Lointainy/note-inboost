1642babd652c791e07d4d3091a763b20c27c9734
Eugene_dev <lointainyyy@gmail.com>
2023-05-09 15:42:30 +0300

    imported new reducers, add middleware

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -4,12 +4,19 @@ import { configureStore } from '@reduxjs/toolkit'
 import noteReducer from './reducers/noteSlice'
 import modalReducer from './reducers/modalSlice'
 import uiReducer from './reducers/uiSlice'
+import { notesApi } from './reducers/noteApi'
+import { authApi } from './reducers/authApi'
+import authReducer from './reducers/authSlice'
 
 export const store = configureStore({
 	reducer: {
+		[authApi.reducerPath]: authApi.reducer,
+		[notesApi.reducerPath]: notesApi.reducer,
 		note: noteReducer,
 		modal: modalReducer,
-		ui: uiReducer
-	}
+		ui: uiReducer,
+		auth: authReducer
+	},
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApi.middleware, notesApi.middleware)
 })
 
