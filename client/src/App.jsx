ba5f9e49ab0cb068cbe7c40d9b55745f6d987c51
Eugene_dev <lointainyyy@gmail.com>
2023-05-09 17:24:37 +0300

    added logic for get notes from api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import authWrapper from './hooks/authWrapper'
 /* Store */
 import { useDispatch, useSelector } from 'react-redux'
 import { setTheme } from './store/reducers/uiSlice'
+import { useGetNotesQuery } from './store/reducers/noteApi'
 
 /* Styles */
 import './App.scss'
@@ -20,6 +21,7 @@ import { NotFoundPage, LoginPage, SignUpPage, NotePage } from './pages'
 /* Components */
 import { Modals } from './components'
 import { Header, Sidebar } from './layout'
+import { setNotes } from './store/reducers/noteSlice'
 
 export const App = () => {
 	const dispatch = useDispatch()
@@ -30,6 +32,9 @@ export const App = () => {
 	// Get name of theme from localStorage or set default
 	const { userTheme } = useTheme()
 
+	// Get data from API
+	const notes = useGetNotesQuery('')
+
 	const { colorTheme: theme, sidebar } = useSelector((store) => store.ui)
 	const userLogined = useSelector((store) => store.auth.login)
 
@@ -37,6 +42,18 @@ export const App = () => {
 		// Set theme name to store
 		userTheme && dispatch(setTheme(userTheme))
 	}, [dispatch, userTheme])
+
+	useEffect(() => {
+		// If user is logged in, fetch boards data and set it to store
+		if (userLogined) {
+			notes.refetch()
+		}
+
+		if (notes.data && notes.isSuccess) {
+			dispatch(setNotes(notes.data))
+		}
+	}, [userLogined, pathname, notes.data, notes.isLoading, notes.isSuccess])
+
 	if (userLogined && !pathname.includes('/login')) {
 		return (
 			<div className={`App ${theme}`}>
