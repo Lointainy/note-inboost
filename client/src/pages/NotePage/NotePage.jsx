76a6843e24c4c2def3ee26905c471bfcc5cd9c32
Eugene_dev <lointainyyy@gmail.com>
2023-05-10 19:53:30 +0300

    added logic for note editing

--- client/src/pages/NotePage/NotePage.jsx
+++ client/src/pages/NotePage/NotePage.jsx
@@ -1,4 +1,74 @@
+import { useEffect, useRef, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { useGetNoteByIdQuery, useUpdateNoteMutation } from '../../store/reducers/noteApi'
+import ReactMarkdown from 'react-markdown'
+import { useToggle } from '../../hooks/useToggle'
+
+/* Styles */
+import style from './NotePage.module.scss'
+
 export default function NotePage() {
-	return <div>NotePage</div>
+	const { noteId } = useParams()
+
+	const { toggle, setToggle, handleToggle } = useToggle(false)
+
+	const [updateNote] = useUpdateNoteMutation()
+
+	const noteRef = useRef({})
+
+	const noteFromApi = useGetNoteByIdQuery(noteId)
+
+	const [note, setNote] = useState({})
+
+	const handleChange = (e) => {
+		const newNote = { ...note, body: e.target.value }
+		setNote(newNote)
+		updateNote({ id: noteId, note: newNote })
+	}
+
+	const handleKeyDown = (e) => {
+		if (e.key === 'Escape') {
+			setToggle(false)
+		} else if (e.key === 'Enter' && e.ctrlKey) {
+			setToggle(false)
+		}
+	}
+
+	useEffect(() => {
+		if (noteFromApi.data) {
+			setNote(noteFromApi.data)
+		}
+	}, [noteFromApi.data, noteFromApi.isSuccess])
+
+	useEffect(() => {
+		const handleOutsideClick = (event) => {
+			if (noteRef.current && !noteRef.current.contains(event.target)) {
+				setToggle(false)
+			}
+		}
+		document.addEventListener('mousedown', handleOutsideClick)
+
+		return () => {
+			document.removeEventListener('mousedown', handleOutsideClick)
+		}
+	}, [])
+
+	return (
+		<div className={style.page}>
+			{noteFromApi.isLoading ? (
+				<span className={style.msg}>Loading...</span>
+			) : (
+				<div className={style.note} ref={noteRef}>
+					{toggle ? (
+						<textarea value={note?.body} onChange={handleChange} className={style.textarea} autoFocus onKeyDown={handleKeyDown}></textarea>
+					) : (
+						<div onClick={handleToggle} className={style.textfield}>
+							<ReactMarkdown>{note?.body}</ReactMarkdown>
+						</div>
+					)}
+				</div>
+			)}
+		</div>
+	)
 }
 
