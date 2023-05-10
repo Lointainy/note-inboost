24bd0e46cab3b03a53947bfe8afd889548b2d2d4
Eugene_dev <lointainyyy@gmail.com>
2023-05-10 19:52:20 +0300

    added route logic

--- client/src/layout/Sidebar/ListItem/index.jsx
+++ client/src/layout/Sidebar/ListItem/index.jsx
@@ -2,14 +2,22 @@ import moment from 'moment'
 
 /* Styles */
 import style from './ListItem.module.scss'
+import { useNavigate } from 'react-router-dom'
 
 export default function ListItem({ note }) {
 	const createdDate = moment(note.updatedAt)
 
+	const navigate = useNavigate()
+
 	const isToday = moment().isSame(createdDate, 'day')
 	const displayDate = isToday ? createdDate.format('HH:mm') : createdDate.format('MMM DD')
+
+	const handleOpenNote = (id) => {
+		navigate(`/note/${id}`)
+	}
+
 	return (
-		<li className={style.item}>
+		<li className={style.item} onClick={() => handleOpenNote(note._id)}>
 			<div className={style.title}>{note.title}</div>
 			<ul className={style.filter}>
 				<li>{note.filter}</li>
