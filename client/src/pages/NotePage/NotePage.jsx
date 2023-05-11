51e63daa58cf8f333cb3f0decf524a03bcbd8c16
Eugene_dev <lointainyyy@gmail.com>
2023-05-11 19:12:10 +0300

    refactored component and logic

--- client/src/pages/NotePage/NotePage.jsx
+++ client/src/pages/NotePage/NotePage.jsx
@@ -6,6 +6,7 @@ import { useParams } from 'react-router-dom'
 /* Store */
 import { useDispatch } from 'react-redux'
 import { useGetNoteByIdQuery, useUpdateNoteMutation } from '../../store/reducers/noteApi'
+import { setAcitveNote } from '../../store/reducers/noteSlice'
 
 /* Hooks */
 import { useToggle } from '../../hooks/useToggle'
@@ -15,13 +16,16 @@ import style from './NotePage.module.scss'
 
 /* Components */
 import ReactMarkdown from 'react-markdown'
-import { setAcitveNote } from '../../store/reducers/noteSlice'
+
+/* Icons */
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
 
 export default function NotePage() {
 	const dispatch = useDispatch()
+
 	const { noteId } = useParams()
 
-	const { toggle, setToggle, handleToggle } = useToggle(false)
+	const { toggle: editing, setToggle: setEditing, handleToggle: handleToggleEditing } = useToggle(false)
 
 	const [updateNote] = useUpdateNoteMutation()
 
@@ -39,9 +43,9 @@ export default function NotePage() {
 
 	const handleKeyDown = (e) => {
 		if (e.key === 'Escape') {
-			setToggle(false)
+			setEditing(false)
 		} else if (e.key === 'Enter' && e.ctrlKey) {
-			setToggle(false)
+			setEditing(false)
 		}
 	}
 
@@ -56,7 +60,7 @@ export default function NotePage() {
 	useEffect(() => {
 		const handleOutsideClick = (event) => {
 			if (noteRef.current && !noteRef.current.contains(event.target)) {
-				setToggle(false)
+				setEditing(false)
 			}
 		}
 		document.addEventListener('mousedown', handleOutsideClick)
@@ -72,10 +76,27 @@ export default function NotePage() {
 				<span className={style.msg}>Loading...</span>
 			) : (
 				<div className={style.note} ref={noteRef}>
-					{toggle ? (
-						<textarea value={note?.body} onChange={handleChange} className={style.textarea} autoFocus onKeyDown={handleKeyDown}></textarea>
+					{editing ? (
+						<>
+							<div className={style.note__head}>
+								<p className={style.hint}>
+									Press <span className={style.hint__span}>Esc</span> or <span className={style.hint__span}>Enter + ctrl</span> for the end
+									editing
+								</p>
+								<button onClick={handleToggleEditing} className={style.close}>
+									<Icon icon="circle-xmark" />
+								</button>
+							</div>
+							<textarea
+								value={note?.body}
+								onChange={handleChange}
+								className={style.textarea}
+								autoFocus
+								onKeyDown={handleKeyDown}
+							></textarea>
+						</>
 					) : (
-						<div onClick={handleToggle} className={style.textfield}>
+						<div onClick={handleToggleEditing} className={style.textfield}>
 							<ReactMarkdown>{note?.body}</ReactMarkdown>
 						</div>
 					)}
