73c18c2ace00bdf6d76af8d442bed56fc2e93dcf
Eugene_dev <lointainyyy@gmail.com>
2023-05-11 22:00:29 +0300

    fixed logic

--- client/src/components/EditorNote/EditorNote.jsx
+++ client/src/components/EditorNote/EditorNote.jsx
@@ -1,19 +1,23 @@
 /* Styles */
 import style from './EditorNote.module.scss'
 
-/* Icons */
-import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
+/* Component */
+import { DropdownOptions } from '../../components'
 
-export default function EditorNote({ handleToggle, onChange, onKeyDown, body }) {
+export default function EditorNote({ handleToggle, onChange, onKeyDown, body, handleOption }) {
+	const options = [
+		{ label: 'Add heading', onClick: () => handleOption('AddHeading') },
+		{ label: 'Add task', onClick: () => handleOption('AddTask') },
+		{ label: 'Close editing', error: true, onClick: () => handleOption('Close') }
+	]
+
 	return (
 		<div className={style.note}>
 			<div className={style.note__head}>
 				<p className={style.hint}>
 					Press <span className={style.hint__span}>Esc</span> or <span className={style.hint__span}>Enter + ctrl</span> for the end editing
 				</p>
-				<button onClick={handleToggle} className={style.close}>
-					<Icon icon="circle-xmark" />
-				</button>
+				<DropdownOptions options={options} fieldStyle={'invert'} />
 			</div>
 			<textarea value={body} onChange={onChange} className={style.textarea} autoFocus onKeyDown={onKeyDown}></textarea>
 		</div>
