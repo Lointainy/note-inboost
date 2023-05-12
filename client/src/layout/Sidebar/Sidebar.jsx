598014080708a4bc3d537605f5a66a060ecb6df5
Eugene_dev <lointainyyy@gmail.com>
2023-05-12 11:34:05 +0300

    added logic for show more

--- client/src/layout/Sidebar/Sidebar.jsx
+++ client/src/layout/Sidebar/Sidebar.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react'
 /* Router */
 import { useNavigate } from 'react-router-dom'
 
+/* Hooks */
+import { useToggle } from '../../hooks/useToggle'
+
 /* Store */
 import { useSelector } from 'react-redux'
 
@@ -12,12 +15,19 @@ import style from './Sidebar.module.scss'
 /* Components */
 import ListItem from './ListItem'
 
+/* Icons */
+import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome'
+
 export default function Sidebar() {
 	const { notes } = useSelector((store) => store.note)
 
 	const navigate = useNavigate()
 
 	const [activeId, setActiveId] = useState('')
+
+	const { toggle: showAllNotes, handleToggle: toggleShowNotes } = useToggle(false)
+
+	const displayNotes = showAllNotes ? notes : notes.slice(0, 5)
 
 	const handleOpenNote = (id) => {
 		navigate(`/note/${id}`)
@@ -27,10 +37,22 @@ export default function Sidebar() {
 	return (
 		<div className={style.sidebar}>
 			<ul className={style.list}>
-				{notes?.map((note) => {
+				{displayNotes?.map((note) => {
 					const isActive = note._id === activeId
 					return <ListItem note={note} key={note._id} isActive={isActive} handleClick={handleOpenNote} />
 				})}
+				{!showAllNotes && notes.length > 5 && (
+					<li className={`${style.show}`} onClick={toggleShowNotes}>
+						<span>{`+${notes.length - 5}`} more</span>
+						<Icon icon="chevron-down" />
+					</li>
+				)}
+				{showAllNotes && notes.length > 5 && (
+					<li className={style.show} onClick={toggleShowNotes}>
+						<span>Show less</span>
+						<Icon icon="chevron-up" />
+					</li>
+				)}
 			</ul>
 			{!notes.length && <div className={style.hint}>Please create your new note</div>}
 		</div>
