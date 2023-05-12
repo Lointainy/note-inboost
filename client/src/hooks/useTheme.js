7ab94873723fa5b751297a0abd8c83f084bd6a9b
Eugene_dev <lointainyyy@gmail.com>
2023-05-12 12:19:03 +0300

    .

--- client/src/hooks/useTheme.js
+++ client/src/hooks/useTheme.js
@@ -1,23 +1,28 @@
 import { useEffect, useState } from 'react'
 
+// get value from localstorage
 export const getValue = () => {
 	const initialValue = localStorage.getItem('user-theme') === 'light-theme' ? false : true
 	return initialValue
-} // get value from localstorage
+}
 
 export function useTheme() {
-	const [theme, setTheme] = useState(getValue) // dark is true
-	const [userTheme, setUserTheme] = useState(localStorage.getItem('user-theme') || 'light-theme')
-	//theme from local or default 'light'
+	// theme, dark is true
+	const [theme, setTheme] = useState(getValue)
 
+	//theme from local or default 'light'
+	const [userTheme, setUserTheme] = useState(localStorage.getItem('user-theme') || 'light-theme')
+
+	// switch theme
 	const toggleTheme = () => {
 		setTheme(!theme)
 		userTheme === 'dark-theme' ? setUserTheme('light-theme') : setUserTheme('dark-theme')
-	} // switch
+	}
 
 	useEffect(() => {
+		// check and add to local
 		localStorage.setItem('user-theme', userTheme)
-	}, [userTheme]) // add to local
+	}, [userTheme])
 
 	return { theme, userTheme, toggleTheme }
 }
