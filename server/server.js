d6ba6ae8b507d78f8d2c7cbf48add6f0c36b99b9
Eugene_dev <lointainyyy@gmail.com>
2023-05-13 19:47:01 +0300

    .

--- server/server.js
+++ server/server.js
@@ -1,20 +1,23 @@
 const express = require('express')
-
 const cors = require('cors')
-
 const dotenv = require('dotenv').config()
-
 const connectDB = require('./config/connectDB')
-
 const swaggerUI = require('swagger-ui-express')
 const { swaggerDoc, optionDoc } = require('./doc/documentation')
-
 const userRoutes = require('./routes/user')
 const noteRoutes = require('./routes/note')
 
 /* Initial app */
 const app = express()
 const PORT = process.env.PORT || 4000
+
+// Home Page
+app.get('/', (req, res) => {
+	res.redirect('/docs')
+})
+
+/* Route for SwaggerUi */
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc, optionDoc))
 
 /* Cors */
 
@@ -36,14 +39,6 @@ app.use((req, res, next) => {
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
-// Home Page
-app.get('/', (req, res) => {
-	res.redirect('/docs')
-})
-
-/* Route for SwaggerUi */
-app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc, optionDoc))
 
 app.use('/api/user', userRoutes)
 app.use('/api/notes', noteRoutes)
