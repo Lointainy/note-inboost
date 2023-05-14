f2d4e6f2dfb5fb0f5dc867838c65e20df5b3ec80
Eugene_dev <lointainyyy@gmail.com>
2023-05-14 15:05:31 +0300

    added utils

---
+++ client/src/utils/patterns.js
@@ -0,0 +1,5 @@
+export const patternEmail = '[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,}$'
+export const patternPassword = '^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=[\\]{};:\\\\|,.<>\\/?])(?!.*\\s).{6,16}$'
+export const patternNoteTitle = ''
+export const patternFilterTitle = ''
+
