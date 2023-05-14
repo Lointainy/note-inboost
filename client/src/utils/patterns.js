97280041a5a38e8d0412737bea0633fbee294ea7
Eugene_dev <lointainyyy@gmail.com>
2023-05-14 15:12:52 +0300

    update

--- client/src/utils/patterns.js
+++ client/src/utils/patterns.js
@@ -1,5 +1,5 @@
 export const patternEmail = '[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,}$'
 export const patternPassword = '^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=[\\]{};:\\\\|,.<>\\/?])(?!.*\\s).{6,16}$'
-export const patternNoteTitle = ''
-export const patternFilterTitle = ''
+export const patternNoteTitle = '^[A-Za-zА-Яа-яІіЇїЄєҐґ -]{3,24}$'
+export const patternFilterTitle = '^[A-Za-zА-Яа-яІіЇїЄєҐґ]{3,24}$'
 
